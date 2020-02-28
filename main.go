7b98d8c79947721e0c504053076e02ae960ea019
math <mathilde.grapin@gmail.com>
2020-02-28 22:47:50 +0100

    :sparkles: Adding router

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"github.com/finalKickOfTheDeath/quado-api/handler"
+	"github.com/finalKickOfTheDeath/quado-api/storage"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost"}
+	router.Use(cors.New(config))
+
+	storage := storage.NewStorage(storage.NewConfig("quado.db", 0600))
+
+	handler := handler.NewHandler(storage)
+
+	storage.Open()
+	storage.InitBuckets()
+
+	router.POST("/boards", handler.CreateBoard)
+	router.DELETE("/boards/:id", handler.DeleteBoard)
+
+	return router
+}
+
 func main() {
 
+	r := setupRouter()
+
+	r.Run(":8080")
 }
